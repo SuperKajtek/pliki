88f1de2ed355ecdd7fedc5a82176c15d8325af19
unknown <kajetan.teodorczyk@gmail.com>
2013-07-04 21:22:57 +0000

    japierdole

---
+++ klasa.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.Callable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kajtek
+ * Date: 04.07.13
+ * Time: 19:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class ppp {
+}
